0c8eba2b0b102835adcd56b8da7fb9bcc58ab4c6
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-02-07 23:27:02 +0900

    [add] CleanUp追加

---
+++ basic_lesson/src/CleanUp.tsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+// import { incrementAsync } from "./features/counter/counterSlice";
+
+const CleanUp: React.FC = () => {
+  const [currentNum, setCurrentNum] = useState(0);
+  const incrementNum = () => {
+    console.log("incrementNum");
+    setCurrentNum(preNumber => preNumber + 1);
+  };
+  useEffect(() => {
+    console.log("CleanUp useEffect");
+    window.addEventListener("mousedown", incrementNum);
+  }, []);
+  return <div>{currentNum}</div>;
+};
+
+export default CleanUp;
