5b6f07870e62e71af8ef19454c73e19611acdd3f
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-02-08 15:00:05 +0900

    [add] signin,signout機能追加

--- src/componets/Auth.tsx
+++ src/componets/Auth.tsx
@@ -1,17 +1,26 @@
-import React from "react";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
+import React, { useState } from "react";
+import styles from "./Auth.module.css";
+import { useDispatch } from "react-redux";
+import { auth, provider, storage } from "../firebase";
+import {
+  Avatar,
+  Button,
+  CssBaseline,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Link,
+  Grid,
+  Box,
+  Typography,
+  Container,
+} from "@material-ui/core";
+import SendIcon from "@material-ui/icons/Send";
+import CameraIcon from "@material-ui/icons/Camera";
+import EmailIcon from "@material-ui/icons/Email";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
 
 const useStyles = makeStyles(theme => ({
   paper: {
@@ -35,7 +44,9 @@ const useStyles = makeStyles(theme => ({
 
 const Auth: React.FC = () => {
   const classes = useStyles();
-
+  const signInGoogle = async () => {
+    await auth.signInWithPopup(provider).catch(err => console.log(err.message));
+  };
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
@@ -69,10 +80,6 @@ const Auth: React.FC = () => {
             id="password"
             autoComplete="current-password"
           />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
           <Button
             type="submit"
             fullWidth
@@ -82,18 +89,16 @@ const Auth: React.FC = () => {
           >
             Sign In
           </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
+
+          <Button
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={signInGoogle}
+          >
+            SignIn with Google
+          </Button>
         </form>
       </div>
     </Container>
--- src/firebase.ts
+++ src/firebase.ts
@@ -3,39 +3,29 @@ import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
 
-const {
-  REACT_APP_FIREBASE_API_KEY,
-  REACT_APP_FIREBASE_AUTH_DOMAIN,
-  // REACT_APP_FIREBASE_DATABASE_URL,
-  REACT_APP_FIREBASE_PROJECT_ID,
-  REACT_APP_FIREBASE_STORAGE_BUCKET,
-  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  REACT_APP_FIREBASE_APP_ID,
-  REACT_APP_FIREBASE_MEASUREMENT_ID,
-} = process.env;
+// const {
+//   REACT_APP_FIREBASE_API_KEY,
+//   REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   // REACT_APP_FIREBASE_DATABASE_URL,
+//   REACT_APP_FIREBASE_PROJECT_ID,
+//   REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   REACT_APP_FIREBASE_APP_ID,
+//   REACT_APP_FIREBASE_MEASUREMENT_ID,
+// } = process.env;
 
 const firebaseConfig = {
-  apiKey: REACT_APP_FIREBASE_API_KEY,
-  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  apiKey: "AIzaSyAAZpUj4snhYi_10zmvum-9EVDdHu0jsyA",
+  authDomain: "amigos-744c8.firebaseapp.com",
   databaseURL: "https://amigos-744c8-default-rtdb.firebaseio.com",
-  projectId: REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: REACT_APP_FIREBASE_APP_ID,
-  measurementId: REACT_APP_FIREBASE_MEASUREMENT_ID,
+  projectId: "amigos-744c8",
+  storageBucket: "amigos-744c8.appspot.com",
+  messagingSenderId: "1062653024190",
+  appId: "1:1062653024190:web:943c9029bb14321bab8bef",
 };
-firebase.initializeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-export const db = firebase.firestore();
+export const db = firebaseApp.firestore();
 export const auth = firebase.auth();
 export const storage = firebase.storage();
 export const provider = new firebase.auth.GoogleAuthProvider();
-
-// // Get a reference to the database service
-// const database = firebase.database();
-// export const messageRef = database.ref("message");
-
-// // firebaseにデータを保存する関数
-// export const pushMessage = ({ name, text }) => {
-//   messageRef.push({ name, text });
-// };
